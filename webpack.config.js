5f41b7256cf85ed60e4c364658f86e44254985ad
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2020-09-14 19:16:44 -0500

    :sparkles: added favicon, ran prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,45 @@
-const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-	devtool: "inline-source-map",
-	entry: __dirname + '/src/js/sketch.ts',
-	output: {
-		path: __dirname + '/dist/',
-		publicPath: '/',
-		filename: 'js/sketch.js'
-	},
-	resolve: { extensions: ['*', '.js', '.jsx', '.ts', '.tsx'] },
-	devServer: {
-		inline: true,
-		hot: true,
-		port: 3333
-	},
-	module: {
-		rules: [{
-			test: /\.(ts|js)x?$/i,
-			exclude: /(node_modules)/,
-			loader: 'babel-loader'
-			},
-			{
-				test: /\.scss$/,
-				loader: "style-loader!css-loader!sass-loader"
-			}
-		]
-	},
-	plugins: [
-		new CopyWebpackPlugin({
-			patterns: [{
-					from: 'src/index.html',
-					to: 'index.html'
-				}
-			],
-		})
-	]
-}
+  devtool: "inline-source-map",
+  entry: __dirname + "/src/js/sketch.ts",
+  output: {
+    path: __dirname + "/dist/",
+    publicPath: "/",
+    filename: "js/sketch.js",
+  },
+  resolve: { extensions: ["*", ".js", ".jsx", ".ts", ".tsx"] },
+  devServer: {
+    inline: true,
+    hot: true,
+    port: 3333,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        loader: "style-loader!css-loader!sass-loader",
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "src/index.html",
+          to: "index.html",
+        },
+        {
+          from: "src/assets",
+          to: "assets",
+        },
+      ],
+    }),
+  ],
+};
